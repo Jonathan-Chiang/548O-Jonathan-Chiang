2bbb344d68a7f847ae9646618913a0c37c39ed13
Jonathan-Chiang <jchiang103@gmail.com>
2023-11-16 14:15:22 -0800

    upload tidy lotr datafile and updated Rscript

--- lotr-tidyverse.R
+++ lotr-tidyverse.R
@@ -18,4 +18,18 @@ lotr_untidy <- bind_rows(fship, ttow, rking)
 str(lotr_untidy)
 lotr_untidy
 
+# write the untidy grouped data to a new csv file 
 write_csv(lotr_untidy, "lotr_untidy.csv")
+
+# gather up female and male variables and make new one, gender
+# using pivot_longer
+lotr_tidy <-
+  pivot_longer(lotr_untidy, cols=c(Female, Male), names_to = 'Gender', values_to = 'Words')
+lotr_tidy <- arrange(lotr_tidy, Gender)
+lotr_tidy
+
+# write tidy grouped data to a new csv file
+write_csv(lotr_tidy, "lotr_tidy.csv")
+
+
+

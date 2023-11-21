e68d5248beb6e1d0a8255120b5598bbb4edc59a8
Jonathan-Chiang <jchiang103@gmail.com>
2023-11-21 14:09:18 -0800

    split exercise 3 into it's on Rscript. update using-dplyr.R

--- using-dplyr.R
+++ using-dplyr.R
@@ -21,25 +21,8 @@ surveys_plus <- mutate(surveys,
 surveys <- mutate(surveys,
                   hindfoot_length_cm = hindfoot_length / 10)
 
-### Exercise 3 ###
-shrub_volume <- read.csv("data/shrub-volume-data.csv")
 
-# check column names in data set 
-names(shrub_volume)
 
-# check structure of data frame
-str(shrub_volume)
+# group dataframe by 
 
-# select data from length column
-select(shrub_volume, length)
 
-# select data from site and experiment columns
-select(shrub_volume, site, experiment)
-
-# filter data from all the plants with height grater than 5
-filter(shrub_volume, height > 5)
-
-# create a tibble that includes all the original data and a new column containing volume
-shrub_data_w_vols <- mutate(shrub_volume,
-                            volume = length * width * height)
-shrub_data_w_vols

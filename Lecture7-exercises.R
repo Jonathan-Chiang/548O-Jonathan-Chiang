0acbfea50d85fea34f675912142789fe314bb9f6
Jonathan-Chiang <jchiang103@gmail.com>
2023-11-30 14:21:57 -0800

    exercise 4

--- Lecture7-exercises.R
+++ Lecture7-exercises.R
@@ -41,10 +41,21 @@ get_mass_from_length_w_defaults <- function(length, a=39.9, b=2.6){
 # estimate mass of sauropoda2 (a = 214.44, b = 1.46) that is 22 m long
 mass_sauropoda2 <- get_mass_from_length_w_defaults(22, 214.44, 1.46); mass_sauropoda2
 
-#
+# estimate mass of unknown dinosaur that is 16m long
+mass_unknown_dino <- get_mass_from_length_w_defaults(16); mass_unknown_dino
 
 
+### Exercise 4 
+library(dplyr)
 
+# function to convert kilograms to pounds (pound=kilogram/2.205)
+kilograms2pounds <- function(kilograms) {
+  pounds <- kilograms/2.205
+  return(pounds)
+}
 
+# estimate weight in pounds for a 12m long stegosaurus (a=10.95, b=2.64)
+mass_stegosaurus <- get_mass_from_length_w_defaults(length=12, a=10.95, b=2.64) %>%
+  kilograms2pounds() ; mass_stegosaurus
 
 

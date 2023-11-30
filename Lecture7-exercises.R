af6e6b17ea1f31b81cdbed3d8814101ba6a858c8
Jonathan-Chiang <jchiang103@gmail.com>
2023-11-30 14:04:04 -0800

    exercise 1

---
+++ Lecture7-exercises.R
@@ -0,0 +1,14 @@
+### Exercise 1 
+
+# write function
+pounds2grams <- function(pounds) {
+  grams <- pounds*453.592
+}
+
+# try function with 3.75 pounds as input
+pound_in_grams <- pounds2grams(3.75); pound_in_grams
+
+#check answer
+print(3.75*453.592)
+
+

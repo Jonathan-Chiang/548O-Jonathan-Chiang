456e1231bfcc470fe3166b6bb725c2a627ddd7ff
Jonathan-Chiang <jchiang103@gmail.com>
2023-11-30 14:15:02 -0800

    exercise 3

--- Lecture7-exercises.R
+++ Lecture7-exercises.R
@@ -32,5 +32,19 @@ return(mass)
 # estimate mass for sauropoda (a = 214.44, b = 1.46) that is 26 m long
 mass_sauropoda <- get_mass_from_length(26, 214.44, 1.46); mass_sauropoda
 
+### Exercise 3 
+get_mass_from_length_w_defaults <- function(length, a=39.9, b=2.6){
+  mass <-  a * length ^ b
+  return(mass)
+}
+
+# estimate mass of sauropoda2 (a = 214.44, b = 1.46) that is 22 m long
+mass_sauropoda2 <- get_mass_from_length_w_defaults(22, 214.44, 1.46); mass_sauropoda2
+
+#
+
+
+
+
 
 

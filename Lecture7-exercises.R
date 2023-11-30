1df828ced14f734d5d4a3a1e3bbc67a83f8c1e30
Jonathan-Chiang <jchiang103@gmail.com>
2023-11-30 14:10:21 -0800

    exercise 2

--- Lecture7-exercises.R
+++ Lecture7-exercises.R
@@ -12,3 +12,25 @@ pound_in_grams <- pounds2grams(3.75); pound_in_grams
 print(3.75*453.592)
 
 
+### Exercise 2
+
+# function estimating mass based on length for Theropoda
+get_mass_from_length_theropoda <- function(length){
+  mass <- 0.73 * length ^ 3.63
+  return(mass)
+}
+
+# estimate mass for Spinosaurus that is 16m long 
+mass_spinosaurus <- get_mass_from_length_theropoda(16); mass_spinosaurus
+
+# 
+get_mass_from_length <- function(length, a, b){
+mass <-  a * length ^ b
+return(mass)
+}
+
+# estimate mass for sauropoda (a = 214.44, b = 1.46) that is 26 m long
+mass_sauropoda <- get_mass_from_length(26, 214.44, 1.46); mass_sauropoda
+
+
+

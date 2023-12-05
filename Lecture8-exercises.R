9827aa4144553d954adf95f90f860c3ed4874eac
Jonathan-Chiang <jchiang103@gmail.com>
2023-12-05 14:02:37 -0800

    exercise 1

---
+++ Lecture8-exercises.R
@@ -0,0 +1,22 @@
+### Exercise 1 
+
+w <- 10.2
+x <- 1.3
+y <- 2.8
+z <- 17.5
+colors <- c("red", "blue", "green")
+masses <- c(45.2, 36.1, 27.8, 81.6, 42.4)
+dna1 <- "attattaggaccaca"
+dna2 <- "attattaggaacaca"
+
+# print whether w is greater than 10
+w > 10
+
+# print whether green is in colors
+"green" %in% colors
+
+# print whether x is greater than y
+x > y
+
+# print whether each value in masses is greater than 40
+masses > 40
